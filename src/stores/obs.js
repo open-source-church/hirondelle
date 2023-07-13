f114264e37d7715b9626b23961059af34672481d
Olivier Keshavjee <olivier.keshavjee@gmail.com>
2023-07-13 13:17:09 +0200

    Avec Document?

--- src/stores/obs.js
+++ src/stores/obs.js
@@ -32,7 +32,7 @@ export const useOBS = defineStore('obs', () => {
   const OSCBotBrowserKeepOnAllScenes = ref(true)
 
   const connect = async (ip, port, password) => {
-    var protocol = location.protocol == "https" ? "wss" : "ws"
+    var protocol = document.location.protocol == "https" ? "wss" : "ws"
     var url = `${protocol}://${ip}:${port}`
     disconnect()
     try {
